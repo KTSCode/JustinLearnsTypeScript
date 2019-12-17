65a43d8e42d7e122f7b4beadaa837d2d1119168a
Justin LaForge <justinrlaforge@gmail.com>
2019-12-17 13:17:45 -0800

    Create Justin's New File test

---
+++ justinsNewFile.ts
@@ -0,0 +1,4 @@
+let punchline: string[] = [`I couldn't think of Acorny joke.`,`I figured you'd think I was nutty.`,`everything I came up with was only Oak-Kay`]
+let joke: string = `
+    I was trying to come up with an Acorns pun, but ${punchline}
+`;
