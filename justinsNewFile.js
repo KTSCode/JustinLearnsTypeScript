292f4201f5c3a90d21efdb50e24b67f0629620b3
Justin LaForge <justinrlaforge@gmail.com>
2019-12-17 13:42:41 -0800

    Created a joke generator

---
+++ justinsNewFile.js
@@ -0,0 +1,3 @@
+var punchline = ["I couldn't think of Acorny joke.", "I figured you'd think I was nutty.", "everything I came up with was only Oak-Kay"];
+var joke = "\n    I was trying to come up with an Acorns pun, but " + punchline[Math.floor(Math.random() * 3)] + "\n";
+console.log(joke);
--- justinsNewFile.ts
+++ justinsNewFile.ts
@@ -1,4 +1,5 @@
 let punchline: string[] = [`I couldn't think of Acorny joke.`,`I figured you'd think I was nutty.`,`everything I came up with was only Oak-Kay`]
 let joke: string = `
-    I was trying to come up with an Acorns pun, but ${punchline}
+    I was trying to come up with an Acorns pun, but ${punchline[Math.floor(Math.random()*3)]}
 `;
+console.log(joke)
